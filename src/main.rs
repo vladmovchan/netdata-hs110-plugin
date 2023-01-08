96f8d77ff995239857f21c9fc2ded6e5e9662aa8
Vladyslav Movchan <vladislav.movchan@gmail.com>
2023-01-09 00:21:10 +0200

    Produce all dimensions for hardcoded list of ips

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ use netdata_plugin::{collector::Collector, Algorithm, Chart, ChartType, Dimensio
 use std::{env, error, io};
 use tplink_hs1x0::HS110;
 
+#[derive(Debug)]
+struct Device<'a> {
+    ip: &'a str,
+    dimension_prefix: String,
+    hostname: String,
+    hs110: HS110,
+}
+
 fn main() -> Result<(), Box<dyn error::Error>> {
     let delay = env::args()
         .skip(1)
@@ -19,54 +27,66 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let mut writer = io::stdout();
     let mut collector = Collector::new(&mut writer);
 
-    let charts = vec![
-        Chart {
-            type_id: "Smartplugs.current",
-            name: "Current",
-            title: "Current",
-            units: "amps",
-            familiy: "current",
-            context: "smartplugpower.current",
-            charttype: Some(ChartType::line),
-            priority: Some(92000),
-            ..Default::default()
-        },
-        Chart {
-            type_id: "Smartplugs.voltage",
-            name: "Voltage",
-            title: "Voltage",
-            units: "volts",
-            familiy: "voltage",
-            context: "smartplugpower.voltage",
-            charttype: Some(ChartType::line),
-            priority: Some(91000),
-            ..Default::default()
-        },
-        Chart {
-            type_id: "Smartplugs.power",
-            name: "Power",
-            title: "Power",
-            units: "watts",
-            familiy: "power",
-            context: "smartplugpower.power",
-            charttype: Some(ChartType::area),
-            priority: Some(90000),
-            ..Default::default()
-        },
-        Chart {
-            type_id: "Smartplugs.total-consumption",
-            name: "Total",
-            title: "Total consumption",
-            units: "watt-hours",
-            familiy: "consumption",
-            context: "smartplugpower.total",
-            charttype: Some(ChartType::line),
-            priority: Some(94000),
-            ..Default::default()
-        },
+    let charts_and_indexes = vec![
+        (
+            Chart {
+                type_id: "Smartplugs.current",
+                name: "Current",
+                title: "Current",
+                units: "amps",
+                familiy: "current",
+                context: "smartplugpower.current",
+                charttype: Some(ChartType::line),
+                priority: Some(92000),
+                ..Default::default()
+            },
+            "current_ma",
+        ),
+        (
+            Chart {
+                type_id: "Smartplugs.voltage",
+                name: "Voltage",
+                title: "Voltage",
+                units: "volts",
+                familiy: "voltage",
+                context: "smartplugpower.voltage",
+                charttype: Some(ChartType::line),
+                priority: Some(91000),
+                ..Default::default()
+            },
+            "voltage_mv",
+        ),
+        (
+            Chart {
+                type_id: "Smartplugs.power",
+                name: "Power",
+                title: "Power",
+                units: "watts",
+                familiy: "power",
+                context: "smartplugpower.power",
+                charttype: Some(ChartType::area),
+                priority: Some(90000),
+                ..Default::default()
+            },
+            "power_mw",
+        ),
+        (
+            Chart {
+                type_id: "Smartplugs.total-consumption",
+                name: "Total",
+                title: "Total consumption",
+                units: "watt-hours",
+                familiy: "consumption",
+                context: "smartplugpower.total",
+                charttype: Some(ChartType::line),
+                priority: Some(94000),
+                ..Default::default()
+            },
+            "total_wh",
+        ),
     ];
 
-    let hosts = vec![
+    let ips = vec![
         "192.168.0.191",
         "192.168.0.156",
         "192.168.0.155",
@@ -74,96 +94,91 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         "192.168.0.102",
         "192.168.0.122",
     ];
-    let hs110 = HS110::new("192.168.0.156".to_string());
 
-    let dimensions = vec![
-        Dimension {
-            id: charts[0].name,
-            name: "dim name (hostname)",
-            algorithm: Some(Algorithm::absolute),
-            ..Default::default()
-        },
-        Dimension {
-            id: charts[1].name,
-            name: "dim name (hostname)",
-            algorithm: Some(Algorithm::absolute),
-            ..Default::default()
-        },
-        Dimension {
-            id: charts[2].name,
-            name: "dim name (hostname)",
-            algorithm: Some(Algorithm::absolute),
-            ..Default::default()
-        },
-        Dimension {
-            id: charts[3].name,
-            name: "dim name (hostname)",
-            algorithm: Some(Algorithm::absolute),
-            ..Default::default()
-        },
-    ];
+    let devices = ips
+        .iter()
+        .map(|ip| {
+            let dimension_prefix = ip.replace('.', "_");
+            let hs110 = HS110::new(ip.to_string());
+            let hostname = hs110.hostname().unwrap_or_else(|_| "<unknown>".to_owned());
+            Device {
+                ip,
+                dimension_prefix,
+                hostname,
+                hs110,
+            }
+        })
+        .collect::<Vec<_>>();
 
-    for (chart, dimension) in charts.iter().zip(dimensions.iter()) {
+    for (chart, index) in charts_and_indexes.iter() {
         collector.add_chart(chart)?;
-        collector.add_dimension(chart.type_id, dimension)?;
+        for Device {
+            ip,
+            hostname,
+            dimension_prefix,
+            ..
+        } in devices.iter()
+        {
+            let dimension = Dimension {
+                id: &format!("{dimension_prefix}_{chart_name}", chart_name = chart.name),
+                name: &format!("{hostname} ({ip})"),
+                algorithm: Some(Algorithm::absolute),
+                divisor: match index.find("_m") {
+                    Some(_) => Some(1000),
+                    None => Some(1),
+                },
+                ..Default::default()
+            };
+            collector.add_dimension(chart.type_id, &dimension)?;
+        }
     }
 
     loop {
-        let emeter = hs110.emeter_parsed()?;
-        match emeter.get("current_ma").or_else(|| emeter.get("current")) {
-            Some(value) => {
-                collector.prepare_value(
-                    charts[0].type_id,
-                    dimensions[0].id,
-                    value.as_f64().unwrap() as i64,
-                )?;
+        for Device {
+            hs110,
+            dimension_prefix,
+            hostname,
+            ip,
+        } in devices.iter()
+        {
+            let emeter = match hs110.emeter_parsed() {
+                Ok(res) => res,
+                Err(e) => {
+                    eprintln!(
+                        "Warning: unable to obtain emeter values from {ip} [{hostname}]: {e}"
+                    );
+                    continue;
+                }
+            };
+            for (chart, index) in charts_and_indexes.iter() {
+                match emeter.get(index) {
+                    Some(value) => {
+                        let dimension_id =
+                            format!("{dimension_prefix}_{chart_name}", chart_name = chart.name);
+                        collector.prepare_value(
+                            chart.type_id,
+                            &dimension_id,
+                            value.as_f64().unwrap_or_else(|| {
+                                eprintln!(
+                                    "Warning: unable to parse `{index}` value `{value}` obtained from {ip} [{hostname}]"
+                                );
+                                0.0
+                            }) as i64,
+                        )?;
+                    }
+                    None => {
+                        eprintln!(
+                            "Warning: `{index}` is not available in emeter readings from {ip} [{hostname}]"
+                        );
+                        continue;
+                    }
+                };
             }
-            None => {
-                panic!("NONE0");
-            }
-        };
-        match emeter.get("voltage_mv").or_else(|| emeter.get("voltage")) {
-            Some(voltage) => {
-                collector.prepare_value(
-                    charts[1].type_id,
-                    dimensions[1].id,
-                    voltage.as_f64().unwrap() as i64,
-                )?;
-            }
-            None => {
-                panic!("NONE1");
-            }
-        };
+        }
 
-        match emeter.get("power_mw").or_else(|| emeter.get("power")) {
-            Some(power) => {
-                collector.prepare_value(
-                    charts[2].type_id,
-                    dimensions[2].id,
-                    power.as_f64().unwrap() as i64,
-                )?;
-            }
-            None => {
-                panic!("NONE2");
-            }
-        };
-
-        match emeter.get("total_wh").or_else(|| emeter.get("total")) {
-            Some(value) => {
-                collector.prepare_value(
-                    charts[3].type_id,
-                    dimensions[3].id,
-                    value.as_f64().unwrap() as i64,
-                )?;
-            }
-            None => {
-                panic!("NONE3");
-            }
-        };
-        collector.commit_chart(charts[0].type_id).unwrap();
-        collector.commit_chart(charts[1].type_id).unwrap();
-        collector.commit_chart(charts[2].type_id).unwrap();
-        collector.commit_chart(charts[3].type_id).unwrap();
+        for (chart, _) in charts_and_indexes.iter() {
+            collector.commit_chart(chart.type_id).unwrap();
+        }
 
         std::thread::sleep(std::time::Duration::from_secs(delay));
     }
